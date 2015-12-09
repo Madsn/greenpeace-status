18efb48b545e7b38b7412e2d7a0098f0e53cbc7b
Madsn <m@madsn.net>
2015-12-09 21:07:19 +0100

    Loop through query array

--- config.example.js
+++ config.example.js
@@ -3,7 +3,14 @@ var config = {
 	port: 443,
 	user: "",
 	password: "",
-	projectKey: "MYPROJ"
+	projectKey: "MYPROJ",
+	queries: [
+		{
+			'issuetype': 'Incident',
+			'label': 'Closed by SKAT',
+			'query': 'issuetype = Incident and resolutiondate > startOfDay(-1d) and resolutiondate < startOfDay(-0d) and status = Closed and project = "SKAT DIAS"'
+		}
+	]
 };
 
 module.exports = config;
--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 JiraApi = require('jira').JiraApi;
 config = require('./config');
+util = require('util');
+queries = config.queries;
 
 var jira = new JiraApi('https', config.host, config.port, config.user, config.password, '2');
 
-jira.findIssue(config.projectKey + "-1234", function(error, issue) {
-	if (error) console.error(error);
-	else console.log('Status: ' + issue.fields.status.name);
-});
-
+for (var i in queries) {
+	(function(i) {
+		var label = queries[i].label;
+		var issuetype = queries[i].issuetype;
+		jira.searchJira(queries[i].query, {}, function(error, result) {
+			if (error) console.error(error);
+			else console.log(result.total + ' ' + label + ' ' + issuetype); 
+			//console.log(util.inspect(result, false, null));
+		});
+	})(i);
+}
